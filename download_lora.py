390aeb60de0ba10d76be94e8064ac4878c076b05
Rohan O'Dwyer <166503624+rohanod@users.noreply.github.com>
2025-01-01 14:48:53 +0800

    Update download_lora.py

--- download_lora.py
+++ download_lora.py
@@ -1,9 +1,12 @@
+import os
 from huggingface_hub import hf_hub_download
+
+destination_path = os.path.join(os.getcwd(), "lora.safetensors")
 
 file_path = hf_hub_download(
     repo_id="rjt1221/SD3.5-LORA", 
     filename="lora.safetensors",
+    local_dir=os.getcwd(),
+    local_dir_use_symlinks=False,
     use_auth_token=True
 )
-
-print(f"File downloaded to: {file_path}")
